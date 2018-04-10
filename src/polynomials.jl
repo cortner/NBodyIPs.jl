37b19c5bb559dc7e376de27e8722e6df8b513b29
Genevieve <dusson@ljll.math.upmc.fr>
2018-04-10 18:32:20 +0100

    some tests on the number of different monomials we should get in polys_fourbody.

--- src/polynomials.jl
+++ src/polynomials.jl
@@ -239,7 +239,8 @@ based on all the one-variable Basis_fct in dimension dim.
 ### Example
 ```
 psym_polys_tot(3, ["y^0","y^1","y^2"], "y")
-```
+
+
 """
 
 function psym_polys_tot(dim::Integer, dict, sym; simplify = false)
@@ -343,6 +344,7 @@ function polys_fourbody(dict_len::Integer)
    #    `i` runs from dim_lo to length(dict); this gives all possible
    #    ordered tuples ⇔ multi-variate polynomials of sum-degree between
    #    dim_lo and length(dict)
+
 	for      i in 4:dict_len,     # (sum of tuple is between 4 and dict_len)
             m = dim_lo:dim_hi,   # (length of tuple is between 4 and 6)
             α in collect(partitions(i, m))
@@ -424,4 +426,40 @@ end
 function polys_fourbody(dict, sym; simplify=true)
    A = polys_fourbody(length(dict))
    return gen_fun(A, dict, sym; simplify=simplify)
+end
+
+B = NBodyIPs.polys_fourbody(1) |> display
+B = NBodyIPs.polys_fourbody2(4) |> display
+
+Deg_max = 2:8
+
+Check_nb_basis_fct = zeros(length(Deg_max),2)
+
+for (ideg,deg_max) in enumerate(Deg_max)
+   theory_nb_basis_fct = 0; #do not count [0,0,0,0,0,0]
+   for i=1:deg_max
+      for m = 1:6
+         for α in collect(partitions(i, m))
+            # any terms not included get zeros appended
+            append!(α, zeros(Int, 6 - length(α)))
+            #display(unique(permutations(α)))
+            theory_nb_basis_fct += length(unique(permutations(α)))
+         end
+      end
+   end
+   Check_nb_basis_fct[ideg,1] = theory_nb_basis_fct
+   Check_nb_basis_fct[ideg,2] = sum(length(NBodyIPs.polys_fourbody2(deg_max)[i]) for i=1:length(NBodyIPs.polys_fourbody2(deg_max)))
+end
+
+#the numbers should correspond.
+Check_nb_basis_fct |> display
+
+deg_max = 2
+length(NBodyIPs.polys_fourbody2(deg_max)) |>display
+for i=1:length(NBodyIPs.polys_fourbody2(deg_max))
+   display(NBodyIPs.polys_fourbody2(deg_max)[i][1]')
+end
+
+for i=1:length(NBodyIPs.polys_fourbody(deg_max))
+   display(NBodyIPs.polys_fourbody(deg_max)[i][1]')
 end
