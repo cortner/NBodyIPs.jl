ce251d990c6adac80aa35ac32277f1f8ca7ce789
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-04-13 21:22:27 +0100

    tests passing

--- src/invariants.jl
+++ src/invariants.jl
@@ -1,13 +1,13 @@
 
 module Invariants
 
-using JuLIP, NeighbourLists
+using JuLIP, NeighbourLists, StaticArrays, ForwardDiff
 
 import Base: length
 import JuLIP: cutoff, energy, forces
 import JuLIP.Potentials: evaluate, evaluate_d
 
-export NBody, NBodyIP, PolyInvariants, InvInvariants
+export NBody, NBodyIP, PolyInvariants, InvInvariants, Dictionary
 
 # ==================================================================
 #           INVARIANTS
@@ -23,7 +23,7 @@ lengths in a simplex. The order is lexicographical, i.e.,
 * 4-body: `r::SVector{6}`, order is `r = [r12, r13, r14, r23, r24, r34]`
 * 5-body: `r::SVector{10}`, analogous
 """
-function invariants
+function invariants end
 
 
 """
@@ -32,12 +32,12 @@ Use polynomials in r as the invariants
 struct PolyInvariants
 end
 
-invariants(::PolyInvariants, r::SVector{3, T}) = SVector{3, T}(
+invariants(::PolyInvariants, r::SVector{3, T}) where {T} = SVector{3, T}(
    r[1]+r[2]+r[3],
    r[1]*r[2] + r[1]*r[3] + r[2]*r[3],
    r[1]*r[2]*r[3] )
 
-grad_invariants(::PolyInvariants, r::SVector{3, T}) = SVector{3, SVector{3,T}}(
+grad_invariants(::PolyInvariants, r::SVector{3, T}) where {T} = SVector{3, SVector{3,T}}(
       SVector{3,T}(1,1,1),
       SVector{3,T}(r[2]+r[3], r[1]+r[3], r[1]+r[2]),
       SVector{3,T}(r[2]*r[3], r[1]*r[3], r[1]*r[2])    )
@@ -49,17 +49,17 @@ Use polynomials in r^{-1} as the invariants
 struct InvInvariants
 end
 
-invariants(::InvInvariants, r::SVector{M, T}) = invariants_inv(1 ./ r)
-grad_invariants(::InvInvariants, r::SVector{M, T}) = grad_invariants_inv(1 ./ r)
+invariants(::InvInvariants, r::SVector{M, T}) where {M, T} = invariants_inv(1 ./ r)
+grad_invariants(::InvInvariants, r::SVector{M, T}) where {M, T} = grad_invariants_inv(1 ./ r)
 
-invariants_inv(s::SVector{3, T}) = SVector{3, T}(
+invariants_inv(s::SVector{3, T}) where {T} = SVector{3, T}(
    s[1]+s[2]+s[3],
    s[1]*s[2] + s[1]*s[3] + s[2]*s[3],
    s[1]*s[2]*s[3] )
 
-function grad_invariants_inv(s::SVector{3, T})
+function grad_invariants_inv(s::SVector{3, T}) where {T}
    t = - s.^2
-   return = SVector{3, SVector{3,T}}(
+   return SVector{3, SVector{3,T}}(
       SVector{3,T}(t[1],t[2],t[3]),
       SVector{3,T}(t[1]*(s[2]+s[3]), t[2]*(s[1]+s[3]), t[3]*(s[1]+s[2])),
       SVector{3,T}(t[1]*s[2]*s[3], t[2]*s[1]*s[3], t[3]*s[1]*s[2])    )
@@ -102,8 +102,8 @@ Dictionary
 @inline fcut(D::Dictionary, r::Number) = (r - D.rcut)^2 * (r < D.rcut)
 @inline fcut_d(D::Dictionary, r::Number) = 2 * (r - D.rcut) * (r < D.rcut)
 
-@inline evaluate(D::Dictionary, i, Q) = Q^i
-@inline evaluate_d(D::Dictionary, i, Q) = i * Q^(i-1)
+@inline evaluate(D::Dictionary, i, Q) = @fastmath Q^i
+@inline evaluate_d(D::Dictionary, i, Q) = (i == 0 ? 0.0 : (@fastmath i * Q^(i-1)))
 
 cutoff(D::Dictionary) = D.rcut
 
@@ -118,12 +118,15 @@ function fcut_d(D::Dictionary, r::SVector{M,T}) where {M, T}
    return (2 * f) ./ (r - D.rcut)
 end
 
+Dictionary(T::Type, rcut) = Dictionary(T(), rcut)
+
+
 # ==================================================================
 #           Polynomials of Invariants
 # ==================================================================
 
 
-@pot struct NBody{N, M, T <: AbstractFloat, TI <: Integer, TINV} <: AbstractCalculator
+@pot struct NBody{N, M, T, TI, TINV} <: AbstractCalculator
    t::Vector{NTuple{M, TI}}   # tuples
    c::Vector{T}               # coefficients
    D::Dictionary{TINV, T}
@@ -151,24 +154,32 @@ e.g., if M = 3, α = t[1] is a 3-vector then this corresponds to the basis funct
 """
 NBody
 
+NBody(t::Vector{NTuple{3, TI}}, c, D) where {TI} = NBody(t, c, D, Val(3))
+NBody(t::Vector{NTuple{6, TI}}, c, D) where {TI} = NBody(t, c, D, Val(6))
+NBody(t::Vector{NTuple{10, TI}}, c, D) where {TI} = NBody(t, c, D, Val(10))
+
+
 length(V::NBody) = length(V.t)
 cutoff(V::NBody) = cutoff(V.D)
-bodyorder(V::NBody{N}) = N
-dim(V::NBody{N,M}) = M
+bodyorder(V::NBody{N}) where {N} = N
+dim(V::NBody{N,M}) where {N, M} = M
 
-function evaluate(V::NBody{3, M, T}, r::AbstractVector{T})  where {M, T}
-   @assert length(r) == M == 3
+function evaluate(V::NBody{3, M, T}, r::AbstractVector{TT})  where {M, T, TT}
+   # @assert length(r) == M == 3
    E = 0.0
    D = V.D
    Q = invariants(D, r)         # SVector{NI, T}
    for (α, c) in zip(V.t, V.c)
       E += c * D(α[1], Q[1]) * D(α[2], Q[2]) * D(α[3], Q[3])
    end
-   return E * fcut(D, r)
+   fc = fcut(D, r[1]) * fcut(D, r[2]) * fcut(D, r[3])
+   return E * fc
 end
 
+# evaluate_d(V::NBody{3, M, T}, r::AbstractVector{T}) where {M, T} =
+#    ForwardDiff.gradient( r_ -> V(r_), r )
+
 function evaluate_d(V::NBody{3, M, T}, r::AbstractVector{T}) where {M, T}
-   @assert length(r) == M == 3
    E = zero(T)
    dE = zero(SVector{M, T})
    D = V.D
@@ -181,14 +192,29 @@ function evaluate_d(V::NBody{3, M, T}, r::AbstractVector{T}) where {M, T}
       E += c * f1 * f2 * f3
       dE += c * ((@D D(α[1], Q[1])) * f2 * f3 * dQ[1] +
                  (@D D(α[2], Q[2])) * f1 * f3 * dQ[2] +
-                 (@D D(α[3], Q[3])) * f2 * f3 * dQ[3] )
+                 (@D D(α[3], Q[3])) * f2 * f1 * dQ[3] )
    end
-   fc = fcut(D, r)
-   fc_d = fcut_d(D, r)
+   fc1, fc2, fc3 = fcut(D, r[1]), fcut(D, r[2]), fcut(D, r[3])
+   dfc1, dfc2, dfc3 = fcut_d(D, r[1]), fcut_d(D, r[2]), fcut_d(D, r[3])
+   fc = fc1 * fc2 * fc3
+   fc_d = SVector{3, Float64}(
+      dfc1 * fc2 * fc3, fc1 * dfc2 * fc3, fc1 * fc2 * dfc3) 
    return dE * fc + E * fc_d
 end
 
 
+function energy(V::NBody{3, M, T}, at::Atoms{T}) where {M, T}
+   nlist = neighbourlist(at, cutoff(V))
+   Es = maptosites!(r -> V(r), zeros(length(at)), nbodies(3, nlist))
+   return sum_kbn(Es)
+end
+
+function forces(V::NBody{3, M, T}, at::Atoms{T}) where {M, T}
+   nlist = neighbourlist(at, cutoff(V))
+   return scale!(maptosites_d!(r -> (@D V(r)),
+                               zeros(SVector{3, T}, length(at)),
+                               nbodies(3, nlist)), -1)
+end
 
 # ==================================================================
 #           The Final Interatomic Potential
@@ -216,4 +242,7 @@ function NBodyIP(basis, coeffs, D::Dictionary)
       push!(orders, V_N)  # collect them
    end
    return NBodyIP(orders)
+end
+
+
 end
---
+++ test/test_invariants.jl
@@ -0,0 +1,74 @@
+using NBodyIPs
+using JuLIP, Base.Test, StaticArrays, ForwardDiff, Combinatorics
+using BenchmarkTools
+using NBodyIPs.Invariants
+
+using NBodyIPs.Invariants: invariants, grad_invariants
+using JuLIP.Potentials: evaluate, evaluate_d
+
+ad_invariants(inv, r) = ForwardDiff.jacobian(r_->invariants(inv, r_), r)
+Inv = InvInvariants()
+r = 0.5 + rand(SVector{3,Float64})
+
+println("-------------------------------------------")
+println("   Testing implementation of `invariants`")
+println("-------------------------------------------")
+
+println("[1] Quick profiling:")
+print("     invariants: ")
+@btime invariants($Inv, $r)
+print("grad_inveriants: ")
+@btime grad_invariants($Inv, $r)
+print("  ad_inveriants: ")
+@btime ad_invariants($Inv, $r)
+
+println("[2] Correctness of gradients")
+for n = 1:10
+   r = 0.5 + rand(SVector{3,Float64})
+   @test hcat(grad_invariants(Inv, r)...)' ≈ ad_invariants(Inv, r)
+end
+
+println("[3] Symmetry")
+for n = 1:10
+   r = 0.5 + rand(SVector{3,Float64})
+   Q = invariants(Inv, r)
+   for rπ in permutations(r)
+      @test Q ≈ invariants(Inv, SVector{3}(rπ))
+   end
+end
+
+
+println("----------------------------------------")
+println("   Testing Implementation of `NBody{3}`")
+println("----------------------------------------")
+
+r0 = rnn(:Cu)
+rcut = 3.1 * r0
+D = Dictionary(InvInvariants, rcut)
+
+n = 10
+println("[1] Quick profiling for a 3-body with $n basis functions")
+at = rattle!(bulk(:Cu, cubic=true) * 2, 0.02)
+r = 1.0 + rand(SVector{3, Float64})
+V3 = NBody( [tuple(rand(1:5, 3)...) for n = 1:n], rand(n), D )
+print("     V3: "); @btime evaluate($V3, $r)
+print("  @D V3: "); @btime evaluate_d($V3, $r)
+print("  nlist: "); @btime neighbourlist(at, rcut)
+print(" energy: "); @btime energy(V3, at)
+print(" forces: "); @btime forces(V3, at)
+
+println("[2] finite-difference test on triangles")
+for n = [1, 3]
+   V3 = NBody( [tuple(rand(0:3, 3)...) for n = 1:n], rand(n), D )
+   for _  = 1:10
+      r = 1.0 + rand(SVector{3,Float64})
+      @test (@D V3(r)) ≈ ForwardDiff.gradient(r_ -> V3(r_), r)
+   end
+end
+
+println("[3] finite-difference test on configurations")
+at = rattle!(bulk(:Cu, cubic=true) * (1,2,2), 0.02)
+for n in [1, 3]
+   V3 = NBody( [tuple(rand(0:3, 3)...) for n = 1:n], 0.01 * rand(n), D )
+   @test JuLIP.Testing.fdtest(V3, at)
+end
