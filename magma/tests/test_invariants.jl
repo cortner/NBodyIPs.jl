aa92e36a5d2fec8567a9c08559100aca84389643
Genevieve <dusson@ljll.math.upmc.fr>
2018-05-16 16:52:02 +0100

    some modifications in the test file.

--- magma/tests/test_invariants.jl
+++ magma/tests/test_invariants.jl
@@ -1,5 +1,6 @@
 
-using StaticArrays, BenchmarkTools, Combinatorics
+using StaticArrays, BenchmarkTools, Combinatorics, Base.Test
+using ForwardDiff
 
 include("../data/NB_5_deg_6_non_efficient_invariants.jl")
 include("../data/NB_5_deg_6_invariants.jl")
@@ -8,10 +9,30 @@ include("../data/NB_5_deg_6_invariants.jl")
 include("fastpolys.jl")
 using FastPolys
 
+
+function d_invariants_Q10_check(x)
+   return ForwardDiff.gradient(invariants_Q10_check, x)
+end
+
+all_invariants(r) = vcat(invariants(r)...)
+ad_invariants(r) = ForwardDiff.jacobian(all_invariants, r)
+r = 1.0 + SVector(rand(10)...)
+# dI1, dI2 = invariants_d(r)
+dI1, dI2 = ad_invariants(r)
+display(dI1)
+display(dI2)
+@test [dI1; dI2] ≈ ad_invariants(r)
+print(".")
+
+
 x = @SVector rand(10)
+epsi = 1e-4;
+h = epsi*(@SVector rand(10))
 
 (Primary_slow, Sec_slow, Irr_sec_slow) = invariants_Q10_check(x)
 (Primary_fast,Sec_fast) = invariants(x)
+gradi = d_invariants_check(x)
+
 
 # ------------------
 # Invariant check vs slow version
