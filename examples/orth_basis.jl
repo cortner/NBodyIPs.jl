bd284dbeb8af0db0a3cba50ec2ab50987a2f1640
Genevieve <dusson@ljll.math.upmc.fr>
2018-05-24 17:09:22 +0100

    Tests of "optimal" weights.

--- examples/orth_basis.jl
+++ examples/orth_basis.jl
@@ -6,11 +6,11 @@ path = homedir() * "/Dropbox/PIBmat/Ti_DFTB_Data/Ti_N54_vartemp.xyz"
 
 # Loading the training data
 data = NBodyIPs.Data.read(path)
-data_sm = data[1:10]
+data_sm = data[1:20]
 
 println("generating basis functions...")
 r0 = rnn(:W)
-rcuts = [5.2, 5.2, 4.5]  # 2-body, 3-body, 4-body
+rcuts = [7.2, 5.2, 4.5]  # 2-body, 3-body, 4-body
 degrees = [10, 8, 6]
 TRANSFORM = "@analytic r -> ($r0/r)^3"
 
@@ -23,30 +23,59 @@ B = vcat(NBody(1.0), gen_basis.([2,3,4], dicts, degrees, purify=false)...)
 B3 = B[ find(bodyorder.(B) .< 4) ]
 # normalize_basis!(B3, data_sm)
 
-Φ, _, _ = NBodyIPs.assemble_lsq(B, data_sm; verbose=true, nforces=10)
+Ψ, Y, _ = NBodyIPs.assemble_lsq(B, data_sm; verbose=true, nforces=Inf)
 
-Q, R = qr(Φ)
+Q, R = qr(Ψ)
 A = R' * R
 
+n = size(Q,1)
+m = size(Q,2)
+
+w_vec = (m/n)./diag(Q * Q')
 
 
-@show vecnorm(A - Φ'*Φ, Inf)
 
-display(A[1:13,1:13])
+Wopt = diagm(w_vec)
 
-idx = 12  # this should be a 2-body
-idx = 20  # this should be a pure 3-body
-interesting_b = B3[idx]
-# TODO how do we figure out that this is almost in the 2-body set
-#  Φ[:,idx] almost in the span of Φ[:, 1:11]
-Φ2 = Φ[:,1:11]
-Q2, R2 = qr(Φ2)
-for idx = 12:length(B3)
-   c = Q2' * Φ[:,idx]
-   err = Q2 * c - Φ[:,idx]
-   @show B3[idx].t
-   @show norm(err, Inf) / norm(Φ[:,idx], Inf)
-end
+cstab=0
+
+Aopt = Q' * (Wopt * Q) + cstab * eye(size(R, 1))
+bopt = Q' * (Wopt * Y)
+copt = R \ (Aopt \ bopt)
+
+zopt = Ψ * copt - Y
+rmsopt = sqrt(dot( zopt, zopt) / length(Y))
+
+println("naive rms error on training set with Wopt: ", rms)
+
+Wnaive = eye(length(w_vec),length(w_vec));
+
+Anaive = Q' * (Wnaive * Q) + cstab * eye(size(R, 1))
+bnaive = Q' * (Wnaive * Y)
+cnaive = R \ (Anaive \ bnaive)
+
+znaive = Ψ * cnaive - Y
+rmsnaive = sqrt(dot(Wnaive * znaive, znaive) / length(Y))
+println("naive rms error on training set with W=Id: ", rmsnaive)
+
+
+# @show vecnorm(A - Φ'*Φ, Inf)
+#
+# display(A[1:13,1:13])
+#
+# idx = 12  # this should be a 2-body
+# idx = 20  # this should be a pure 3-body
+# interesting_b = B3[idx]
+# # TODO how do we figure out that this is almost in the 2-body set
+# #  Φ[:,idx] almost in the span of Φ[:, 1:11]
+# Φ2 = Φ[:,1:11]
+# Q2, R2 = qr(Φ2)
+# for idx = 12:length(B3)
+#    c = Q2' * Φ[:,idx]
+#    err = Q2 * c - Φ[:,idx]
+#    @show B3[idx].t
+#    @show norm(err, Inf) / norm(Φ[:,idx], Inf)
+# end
 
 # # [3] do the regression and construct and IP
 # c = regression(B, train_data, nforces = Inf, stab = 0.0)
