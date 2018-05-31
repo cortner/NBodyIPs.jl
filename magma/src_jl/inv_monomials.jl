95a5101ccf65b56f311d57cecc25881fe6680a7d
Genevieve <dusson@ljll.math.upmc.fr>
2018-05-31 14:27:03 +0100

    starting representation of monomials in terms of invariants.

---
+++ magma/src_jl/inv_monomials.jl
@@ -0,0 +1,10 @@
+using Combinatorics, StaticArrays
+include("misc.jl")
+include("invariants_generator.jl")
+
+
+# Determine the representant of a monomial
+
+
+
+# Check duplicates in a list of monomials
