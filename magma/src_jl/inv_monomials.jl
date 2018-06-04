749be2fd7c4cdb7811068e54216e83d850b4b949
Genevieve <dusson@ljll.math.upmc.fr>
2018-06-04 11:08:24 +0100

    few new functions with new types.

--- magma/src_jl/inv_monomials.jl
+++ magma/src_jl/inv_monomials.jl
@@ -10,8 +10,8 @@ import Base: length, +, *
 # know length, mutable : MVector
 # don't know length : Vector
 
-abstract type AbstractMonomial{M} end
-# abstract type CompactMonomial{M} <: AbstractMonomial{M} end
+abstract type AbstractMonList{M} end
+# abstract type CompactMonomial{M} <: AbstractMonList{M} end
 abstract type PolyMonomial{M} end
 
 # struct Monomial{M} <: CompactMonomial{M}
@@ -22,18 +22,17 @@ abstract type PolyMonomial{M} end
 #     a::SVector{M, Int}
 # end
 
-struct Monlist{M} <: AbstractMonomial{M}
+struct MonList{M} <: AbstractMonList{M}
     A::Vector{SVector{M, Int}}
 end
-Monlist(nothing) = Vector{SVector{M, Int}}[]
 
-struct CMonlist{M} <: AbstractMonomial{M}
+struct CMonList{M} <: AbstractMonList{M}
     A::Vector{SVector{M, Int}}
 end
-Mon(m::AbstractMonomial{M}) where {M} = m.A
+Mon(m::AbstractMonList{M}) where {M} = m.A
 
 struct PolyMon{M} <: PolyMonomial{M}
-   mon::Monlist{M} #monomials
+   mon::MonList{M} #monomials
    coef::Vector{Int} #coefficients
 end
 
@@ -41,19 +40,19 @@ Mon(m::PolyMon{M}) where {M} = (m.mon).A
 Coef(m::PolyMon{M}) where {M} = m.coef
 
 struct CPolyMon{M} <: PolyMonomial{M}
-   mon::CMonlist{M} #monomials
+   mon::CMonList{M} #monomials
    coef::Vector{Int} #coefficients
 end
 Mon(m::CPolyMon{M}) where {M} = (m.mon).A
 Coef(m::CPolyMon{M}) where {M} = m.coef
 
 
-dim(m::AbstractMonomial{M}) where {M} = M
+dim(m::AbstractMonList{M}) where {M} = M
 
 
 # lengths redifinitions
-length(m::Monlist) = length(m.A)
-length(m::CMonlist) = length(m.A)
+length(m::MonList) = length(m.A)
+length(m::CMonList) = length(m.A)
 
 length(m::PolyMon) = length(m.coef)
 length(m::CPolyMon) = length(m.coef)
@@ -61,9 +60,8 @@ length(m::CPolyMon) = length(m.coef)
 
 edges2bo(M::Integer) = (M <= 0) ? 1 : round(Int, 0.5 + sqrt(0.25 + 2 * M))
 
-# simplex_permutations(x::Monomial{M}) =
-#    simplex_permutations(Val(edges2bo(M)), x.a)
-
+# defining constant polynomial
+constpoly(M::Integer) = CPolyMon(CMonList([@SVector zeros(Int,M)]),[1])
 
 # Determine the representant of a monomial
 function mon_repr(monomial::SVector{M, Int}) where {M}
@@ -73,35 +71,30 @@ function mon_repr(monomial::SVector{M, Int}) where {M}
 end
 
 
-# Check duplicates in a list of monomials
-function check_dupl(monomiallist::AbstractMonomial)
+# Check duplicates in a list of monomials and remove them
+function remove_dupl(monomiallist::MonList{M}) where {M}
     mon_list = Mon(monomiallist)
-    @show mon_list
-    mon_list_out = Monlist(nothing)
+    mon_list_out = SVector{M, Int}[]
 
     for i=1:length(mon_list)
         if !(mon_list[i] in mon_list_out)
-            @show mon_list[i]
-            @show mon_list_out
             push!(mon_list_out,mon_list[i])
         end
     end
-    @show mon_list_out
-    return Monlist(mon_list_out)
+    return MonList(mon_list_out)
 end
 
-# Check duplicates in a list of monomials and adds the coefficients when duplicates
-function check_dupl_add(monomiallist::PolyMonomial)
+
+# Remove duplicates in a list of monomials and adds the coefficients when duplicates
+function remove_dupl(monomiallist::PolyMon)
     mon_list = Mon(monomiallist)
     coef_list = Coef(monomiallist)
     @assert length(mon_list) == length(coef_list)
-    mon_list_out = []
-    mon_coef_out = []
+    mon_list_out = SVector{M, Int}[]
+    mon_coef_out = Int[]
 
     for i=1:length(mon_list)
-        @show mon_list
         if !(mon_list[i] in mon_list_out)
-            @show mon_list[i]
             push!(mon_list_out,mon_list[i])
             push!(mon_coef_out,coef_list[i])
         else
@@ -109,142 +102,160 @@ function check_dupl_add(monomiallist::PolyMonomial)
             mon_coef_out[ind[1]] += coef_list[i]
         end
     end
-    return PolyMon(Monlist(mon_list_out),mon_coef_out)
+    return PolyMon(MonList(mon_list_out),mon_coef_out)
 end
 
 
-
-
+# Compute the compact form of a list of monomials
+function compact(monomiallist::MonList{M}) where {M}
+    # first simplification - remove duplicate monomials
+    monomiallist_in = remove_dupl(monomiallist)
+    mon_list = Mon(monomiallist_in)
+    mon_list_out = SVector{M, Int}[]
+    for i=1:length(mon_list)
+        if !(mon_repr(mon_list[i]) in mon_list_out)
+            push!(mon_list_out,mon_repr(mon_list[i]))
+        end
+    end
+    return CMonList(mon_list_out)
+end
 
 # Compute the compact form of a list of monomials
-function compact(monomiallist::PolyMonomial)
+function compact(monomiallist::PolyMon{M}) where {M}
     # first simplification - remove duplicate monomials
-    monomiallist_in = check_dupl_add(monomiallist)
+    monomiallist_in = remove_dupl(monomiallist)
     mon_list = Mon(monomiallist_in)
     coef_list = Coef(monomiallist_in)
     @assert length(mon_list) == length(coef_list)
 
-    mon_list_out = []
-    coef_list_out = []
+    mon_list_out = SVector{M, Int}[]
+    coef_list_out = Int[]
 
     for i=1:length(mon_list)
-        if !(monomial_repr(mon_list[i]) in mon_list_out)
+        if !(mon_repr(mon_list[i]) in mon_list_out)
             push!(mon_list_out,mon_repr(mon_list[i]))
-            push!(coef_list_out,coef_list_in[i])
+            push!(coef_list_out,coef_list[i])
         else
             ind = find([mon_repr(mon_list[i]) == mon_repr(mon_list_out[j]) for j=1:length(mon_list_out)])
-            @assert coef_list_in[i] == coef_list_out[ind[1]]
+            @assert coef_list[i] == coef_list_out[ind[1]]
         end
     end
-    return PolyMonomial(mon_list_out,coef_list_out)
+    return CPolyMon(CMonList(mon_list_out),coef_list_out)
 end
 
 
-
-Monlist(nothing)
-
-M = 6
-mon = SVector(1,2,0,0,0,0)
-
-length(mon)
-
-length(Monlist([mon, mon]))
-
-mon2 = Monlist([mon, mon])
-
-length(mon2.A)
-
-mon_repr(mon)
-
-Mono = PolyMon(Monlist([mon, mon]),Vector([1, 2]))
-length(Mono)
-
-mon_repr(mon)
-
-check_dupl_add(Mono::PolyMonomial)
-check_dupl(Monlist([mon, mon]))
-
-compact(Mono)
-
-
-
-
-# Compute the compact form of a list of monomials
-function compact_form_mon(mon_list)
-    # first simplification - remove duplicate monomials
-    mon_list_in = check_dupl(mon_list)
-    mon_list_out = []
-
-    for i=1:length(mon_list_in)
-        if !(monomial_repr(mon_list_in[i]) in mon_list_out)
-            push!(mon_list_out,monomial_repr(mon_list_in[i]))
-        end
-    end
-    return mon_list_out
-end
-
-# Compute the expanded version of a compact list of monomials
-function expanded_form_mon(mon_list,coef_list)
+# Compute the expanded version of a compact list of monomials or polymon
+function expand_mon(monomiallist::CPolyMon{M}) where {M}
+    mon_list = Mon(monomiallist)
+    coef_list = Coef(monomiallist)
     @assert length(mon_list) == length(coef_list)
-    # first simplification - remove duplicate monomials
-    mon_list_in, coef_list_in = check_dupl_add(mon_list,coef_list)
 
-    mon_list_out = []
-    coef_list_out = []
+    mon_list_out = SVector{M, Int}[]
+    coef_list_out = Int[]
 
     for i=1:length(mon_list)
-        USP = unique(simplex_permutations(SVector(mon_list_in[i]...)))
+        USP = unique(simplex_permutations(mon_list[i]))
         append!(mon_list_out,USP)
-        # TODO: more efficient implementation???
-        for j=1:length(USP)
-            push!(coef_list_out,coef_list_in[i])
-        end
+        append!(coef_list_out,collect(Iterators.repeated(coef_list[i],length(USP))))
     end
-    return mon_list_out,coef_list_out
+    return PolyMon(MonList(mon_list_out),coef_list_out)
 end
 
-function prod_mon_comp(mon1_list,coef1_list,mon2_list,coef2_list)
-    mon1_list_ex,coef1_list_ex = expanded_form_mon(mon1_list,coef1_list)
-    mon2_list_ex,coef2_list_ex = expanded_form_mon(mon2_list,coef2_list)
-    mon_list_out,coef_list_out = prod_mon(mon1_list_ex,coef1_list_ex,mon2_list_ex,coef2_list_ex)
-    return compact_form_mon(mon_list_out,coef_list_out)
+
+function expand_mon(monomiallist::CMonList{M}) where {M}
+    mon_list = Mon(monomiallist)
+    mon_list_out = SVector{M, Int}[]
+
+    for i=1:length(mon_list)
+        USP = unique(simplex_permutations(mon_list[i]))
+        append!(mon_list_out,USP)
+    end
+    return MonList(mon_list_out)
 end
 
-# compute the product of two list of monomials
-function prod_mon(mon1_list,coef1_list,mon2_list,coef2_list)
-    mon_list_out = []
-    coef_list_out = []
+
+# compute the product of 2 lists of monomials
+function *(PMon1::CPolyMon{M},PMon2::CPolyMon{M}) where {M}
+    Exp_PMon1 = expand_mon(PMon1)
+    Exp_PMon2 = expand_mon(PMon2)
+    mon1_list = Mon(Exp_PMon1)
+    coef1_list = Coef(Exp_PMon1)
+    mon2_list = Mon(Exp_PMon2)
+    coef2_list = Coef(Exp_PMon2)
+
+    mon_list_out = SVector{M, Int}[]
+    coef_list_out = Int[]
     for i=1:length(mon1_list)
         for j=1:length(mon2_list)
             push!(mon_list_out,mon1_list[i]+mon2_list[j])
             push!(coef_list_out,coef1_list[i]*coef2_list[j])
         end
     end
-    return check_dupl_add(mon_list_out,coef_list_out)
+    return compact(PolyMon(MonList(mon_list_out),coef_list_out))
 end
+
+function *(Mon1::PolyMon{M},PMon2::CPolyMon{M}) where {M}
+    PMon1 = compact(Mon1)
+    return *(PMon1,PMon2)
+end
+
+
+function *(PMon1::CPolyMon{M},Mon2::PolyMon{M}) where {M}
+    return *(Mon2,PMon1)
+end
+
+
+function *(Mon1::PolyMon{M},Mon2::PolyMon{M}) where {M}
+    PMon1 = compact(Mon1)
+    PMon2 = compact(Mon2)
+    return *(PMon1,PMon2)
+end
+
+
 
 # compute the power of a function given by monomials
-# function power(mon_list,coef_list,::Val{1})
-#     return check_dupl_add(mon_list,coef_list)
-# end
-#
-# function power(mon_list,coef_list,::Val{2})
-#     return prod_mon(mon_list,coef_list,mon_list,coef_list)
-# end
-
-# compute the power of a function given by monomials (not compacted)
-function power(mon_list,coef_list,p)
-    if p == 1
-         return check_dupl_add(mon_list,coef_list)
-    elseif p == 2
-         mon_list_out,coef_list_out = prod_mon(mon_list,coef_list,mon_list,coef_list)
-         return check_dupl_add(mon_list_out,coef_list_out)
+function power(PMon::CPolyMon{M},n::Int) where {M}
+    if n == 0
+        return constpoly(M)
+    elseif n == 1
+        return PMon
     else
-        mon_list_temp, coef_list_temp = power(mon_list,coef_list,p-1)
-        mon_list_out,coef_list_out = prod_mon(mon_list,coef_list,mon_list_temp, coef_list_temp)
-        return check_dupl_add(mon_list_out,coef_list_out)
+        return PMon*(power(PMon,n-1))
     end
 end
+
+power(Mon::PolyMon{M},n::Int) where {M} = power(compact(Mon),n)
+
+
+
+# Small tests
+M = 6
+mon = SVector(1,2,0,0,0,0)
+length(mon)
+mon_repr(mon)
+
+listmon = MonList([mon, mon])
+length(listmon)
+remove_dupl(listmon)
+clistmon = compact(listmon)
+expand_mon(clistmon)
+
+Monopoly = PolyMon(MonList([mon, mon]),Vector([1, 2]))
+length(Monopoly)
+remove_dupl(Monopoly)
+cMonopoly = compact(Monopoly)
+expand_mon(cMonopoly)
+cMonopoly*cMonopoly
+cMonopoly*Monopoly
+Monopoly*cMonopoly
+Monopoly*Monopoly
+
+monzero = SVector(0,0,0,0,0,0)
+mon_repr(monzero)
+M0P = CPolyMon(CMonList([monzero, monzero]),Vector([1, 2]))
+M0P*M0P
+
+power(cMonopoly,3)
 
 
 multisets(k, n) = map(A -> [sum(A .== i) for i in 1:n],
@@ -258,8 +269,8 @@ function generate_rep_mon(NBlengths,d)
         Decomp_deg = multisets(deg,NBlengths)
         mon_list_in_deg = compact_form_mon(Decomp_deg)
         for i=1:length(mon_list_in_deg)
-            if !(monomial_repr(mon_list_in_deg[i]) in mon_list_out_deg)
-                push!(mon_list_out_deg,monomial_repr(mon_list_in_deg[i]))
+            if !(mon_repr(mon_list_in_deg[i]) in mon_list_out_deg)
+                push!(mon_list_out_deg,mon_repr(mon_list_in_deg[i]))
             end
         end
         append!(mon_list_out,mon_list_out_deg)
