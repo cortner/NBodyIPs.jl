81afaeeedc0e47a07f57142a770dcc97d48796e1
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-06-28 14:36:35 +0100

    energy, forces, and virial for EnvBL implemented and tested

--- src/environ.jl
+++ src/environ.jl
@@ -1,30 +1,56 @@
 
 module EnvBLs
 
-using JuLIP
+using JuLIP, StaticArrays
+using JuLIP.Potentials: evaluate, evaluate_d
 
+using NBodyIPs: max_neigs, eval_site_nbody!, _grad_len2pos!
 using NBodyIPs.Polys: Dictionary, NBody
 
-abstract type EnvBLFunction{N} <: AbstractCalculator
+abstract type EnvBLFunction{N} <: AbstractCalculator end
+
+import JuLIP: cutoff, energy, forces, virial
+
+import NBodyIPs: NBodyIP, bodyorder, fast,
+                 evaluate_many!, evaluate_many_d!,
+                 saveas, loadas,
+                 saveas_json, loadas_json
 
 
-"""
-An EnvBL is a product of a polynomial in `n` and an
-`NBody`
-"""
-struct EnvBL{N, T, TVR <: NBody{N}, TVN}
+@pot struct EnvBL{N, T, TVR, TVN} <: EnvBLFunction{N}
    t::Vector{Int}
    c::Vector{T}
    Vr::TVR
    Vn::TVN
    valN::Val{N}
+   #
+   # function EnvBL(t, c::Vector{T}, Vr::TVR, Vn::TVN, valN::Val{N}
+   #               ) where {N, T, TVR, TVN}
+   #    if N != bodyorder(Vn)
+   #       error("EnvBL : body-orders must match")
+   #    end
+   #    return new(t, c, Vr, Vn, valN)
+   # end
 end
+
+"""
+An EnvBL is a product of a polynomial in `n` and an
+`NBody`
+"""
+EnvBL
+
+
+EnvBL(t, c, Vr, Vn) = EnvBL(t, c, Vr, Vn, Val(bodyorder(Vr)))
 
 Vn(V::EnvBL) = V.Vn
 Vr(V::EnvBL) = V.Vr
 
-# turn it into a potential
-@pot EnvBL
+bodyorder(V::EnvBLFunction) = bodyorder(Vr(V))
+
+function cutoff(V::EnvBLFunction)
+   @assert cutoff(Vn(V)) <= cutoff(Vr(V))
+   return cutoff(Vr(V::EnvBL))
+end
 
 # evaluate(V::EnvBL, n, r) =
 #    evaluate(V.Vn, n) * evaluate(V.Vr, r)
@@ -37,18 +63,21 @@ n_fun(V::EnvBL, n) = sum( c * n^t  for (c,t) in zip(V.c, V.t) )
 
 n_fun_d(V::EnvBL, n) = sum( t * c * n^(t-1)  for (c,t) in zip(V.c, V.t) )
 
-site_ns(V::EnvBLFunction{N}, at) =
-   nfun.(V, site_energies(Vn(V), at))
+site_ns(V::EnvBL, at) = n_fun.(V, site_energies(Vn(V), at))
 
-function site_n_d!(dVn, V::EnvBLFunction{N}, r, R, Ni)
-   dNi = n_fun_d(Ni)
+function site_ns_ed(V::EnvBL, at)
+   Vns = site_energies(Vn(V), at)
+   return n_fun.(V, Vns), n_fun_d.(V, Vns)
+end
+
+function site_n_d!(dVn, V::EnvBL, r, R, Ni, dNi)
    for n = 1:length(r)
-      dVn[n] = dNi * grad(Vn(V), r[n], R[n])
+      dVn[n] = 0.5 * dNi * evaluate_d(Vn(V), r[n]) * R[n] / r[n]
    end
    return dVn
 end
 
-function energy(V::EnvBLFunction{N}, at::Atoms)
+function energy(V::EnvBLFunction, at::Atoms)
    # compute the n values
    Ns = site_ns(V, at)
    # compute the inner v values
@@ -58,17 +87,17 @@ function energy(V::EnvBLFunction{N}, at::Atoms)
 end
 
 
-function forces(V::EnvBLFunction{N}, at::Atoms)
-   @assert cutoff(V.Vn) <= cutoff(V.Vr)
+function forces(V::EnvBLFunction{N}, at::Atoms{T}) where {N, T}
 
    # compute the n values
-   Ns = site_ns(V, at)
+   Ns, dNs = site_ns_ed(V, at)
    # compute the inner v values
    Vs = site_energies(Vr(V), at)
 
    # now assemble site forces and use those to create
    # total forces by mixing N and V
-   nlist = neighbourlist(at, cutoff(V))
+   cutoff_n = cutoff(Vn(V))
+   nlist = neighbourlist(at, cutoff(V))  # this checks that cutoff(Vn) <= cutoff(Vr)
    maxneigs = max_neigs(nlist)
    F = zeros(JVec{T}, length(at))
    dVsite = zeros(JVec{T}, maxneigs)
@@ -77,11 +106,11 @@ function forces(V::EnvBLFunction{N}, at::Atoms)
    for (i, j, r, R) in sites(nlist)
       dVsite .*= 0.0
       eval_site_nbody!(
-            Val(N), R, cutoff(V),
-            (out, s, S, J, _) -> _grad_len2pos!(out, evaluate_d(V, s)/N, J, S),
+            Val(N), R, cutoff(Vr(V)),
+            (out, s, S, J, _) -> _grad_len2pos!(out, evaluate_d(Vr(V), s)/N, J, S),
             dVsite, nothing )
 
-      site_n_d!(dVn, V, r, R, Ns[i])
+      site_n_d!(dVn, V, r, R, Ns[i], dNs[i])
 
       # write site energy gradient into forces
       for n = 1:length(j)
@@ -91,6 +120,41 @@ function forces(V::EnvBLFunction{N}, at::Atoms)
       end
    end
    return F
+end
+
+
+
+function virial(V::EnvBLFunction{N}, at::Atoms{T}) where {N, T}
+   # compute the n values
+   Ns, dNs = site_ns_ed(V, at)
+   # compute the inner v values
+   Vs = site_energies(Vr(V), at)
+
+   # now assemble site forces and use those to create
+   # total forces by mixing N and V
+   cutoff_n = cutoff(Vn(V))
+   nlist = neighbourlist(at, cutoff(V))  # this checks that cutoff(Vn) <= cutoff(Vr)
+   maxneigs = max_neigs(nlist)
+   dVsite = zeros(JVec{T}, maxneigs)
+   dVn = zeros(JVec{T}, maxneigs)
+
+   S = @SMatrix zeros(3,3)
+
+   for (i, j, r, R) in sites(nlist)
+      dVsite .*= 0.0
+      dVn .*= 0.0
+      eval_site_nbody!(
+            Val(N), R, cutoff(V),
+            (out, s, S, J, _) -> _grad_len2pos!(out, evaluate_d(Vr(V), s)/N, J, S),
+            dVsite, nothing )
+      site_n_d!(dVn, V, r, R, Ns[i], dNs[i])
+      for n = 1:length(j)
+         dVsite[n] = Ns[i] * dVsite[n] + Vs[i] * dVn[n]
+      end
+
+      S += JuLIP.Potentials.site_virial(dVsite, R)
+   end
+   return S
 end
 
 
---
+++ test/test_environ.jl
@@ -0,0 +1,32 @@
+using Base.Test
+
+using NBodyIPs
+const Env = NBodyIPs.EnvBLs
+
+println("Setting up the test systems ...")
+r0 = rnn(:Cu)
+TRANSFORM = let r0 = r0
+   # (@analytic r -> (r0/r)^3)
+   (@analytic r -> exp( - 3 * ((r/r0) - 1)))
+end
+at = rattle!(bulk(:Cu, cubic=true) * 2, 0.02)
+rcut3 = 2.1 * r0
+D3 = Dictionary(TRANSFORM, (:cos, 0.66*rcut3, rcut3) )
+
+random_3body(ntup=1) = NBody( [tuple( [rand(1:4, 3); 0]... ) for n = 1:ntup],
+                              (0.1+rand(ntup))/factorial(3), D3 )
+
+Vn = let r0=r0, rcut=1.5*r0
+   (@analytic r -> exp(- 3 * ((r/r0)-1))) * C1Shift(rcut)
+end
+
+V3env_0 = Env.EnvBL([0,], [1.0+rand()], random_3body(), Vn)
+V3env_1 = Env.EnvBL([1,], [1.0+rand()], random_3body(), Vn)
+V3env_2 = Env.EnvBL([2,], [1.0+rand()], random_3body(), Vn)
+
+println("Finite-difference test on configurations")
+println("------------------------------------------------")
+set_constraint!(at, VariableCell(at))
+for Venv in [V3env_0, V3env_1, V3env_2]
+   (@test JuLIP.Testing.fdtest(Venv, at)) |> println
+end
